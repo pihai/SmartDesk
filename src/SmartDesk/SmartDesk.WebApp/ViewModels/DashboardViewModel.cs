a7be8b29aff843ded4525716ed32a074d8d136b6
f-loris <fabian.loris@gmail.com>
2016-05-31 01:47:45 +0200

    dashboard model change

--- src/SmartDesk/SmartDesk.WebApp/ViewModels/DashboardViewModel.cs
+++ src/SmartDesk/SmartDesk.WebApp/ViewModels/DashboardViewModel.cs
@@ -2,12 +2,14 @@
 
 namespace SmartDesk.WebApp.ViewModels {
   public class DashboardViewModel {
-    public DashboardViewModel(TimeSpan standingToday) {
-      StandingToday = standingToday;
+    public DashboardViewModel(string deviceId, TimeSpan standingTarget) {
+      StandingTarget = standingTarget;
+      DeviceId = deviceId;
     }
 
-    public TimeSpan StandingToday { get; }
-    public string StandingTodayText => TimeSpanToDisplayText(StandingToday);
+    public TimeSpan StandingTarget { get; }
+    public string DeviceId { get; }
+    public string StandingTodayText => TimeSpanToDisplayText(StandingTarget);
 
     private static string TimeSpanToDisplayText(TimeSpan t) => $"{t.Hours} h {t.Minutes} min";
   }
