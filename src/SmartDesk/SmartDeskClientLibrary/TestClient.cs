2dd141c38b3e9fad0eb253e2e91850b9c29a0751
f-loris <fabian.loris@gmail.com>
2016-05-30 22:10:41 +0200

    test client impl

---
+++ src/SmartDesk/SmartDeskClientLibrary/TestClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartDesk.Client.Arduino
+{
+    public class TestClient : ISmartDeskClient
+    {
+        private Random rand;
+        public TestClient(int seed)
+        {
+            rand = new Random(seed);
+        }
+        public TestClient()
+        {
+            rand = new Random();
+        }
+
+        public int GetHeight()
+        {
+            if (rand.Next(0, 100) > 50)
+                return rand.Next(65, 75);
+            return rand.Next(95, 110);
+        }
+
+        public bool IsConnected()
+        {
+            return true;
+        }
+
+        public bool TryGetHeight(out int height)
+        {
+
+            height = GetHeight();
+            return true;
+        }
+
+
+    }
+}
--- src/SmartDesk/SmartDeskConsoleClient/Program.cs
+++ src/SmartDesk/SmartDeskConsoleClient/Program.cs
@@ -6,6 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.Devices.Client;
 using Newtonsoft.Json;
+using SmartDesk.Client;
+using SmartDesk.Client.Arduino;
+using Microsoft.Win32;
 
 namespace SmartDesk.Client
 {
@@ -14,10 +17,32 @@ namespace SmartDesk.Client
         private static DeviceClient _deviceClient;
         private static readonly string IotHubUri = ConfigurationManager.AppSettings["IotHubUri"];
         private static readonly string DeviceKey = ConfigurationManager.AppSettings["deviceKey"];
-
+        private static ISmartDeskClient client;
+        private static bool IsActive = true;
         static void Main(string[] args)
         {
+            client = new TestClient();
+            string test = ConfigurationManager.AppSettings["deviceKey"];
             Console.WriteLine("Simulated device");
+            SystemEvents.SessionSwitch += (sender, e) =>
+
+
+               {
+                   if (e.Reason == SessionSwitchReason.SessionLock)
+                   {
+                       //I left my desk
+                       IsActive = false;
+                       Console.WriteLine("Left desk");
+                   }
+                   else if (e.Reason == SessionSwitchReason.SessionUnlock)
+                   {
+                       //I returned to my desk
+                       IsActive = true;
+                       Console.WriteLine("Returned to desk");
+                   }
+               };
+
+
             _deviceClient = DeviceClient.Create(IotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("device1", DeviceKey));
 
             SendDeviceToCloudMessagesAsync();
@@ -26,19 +51,22 @@ namespace SmartDesk.Client
 
         private static async void SendDeviceToCloudMessagesAsync()
         {
-            var avgHeight = 50;
-            var rand = new Random();
+            //var avgHeight = 50;
+            //var rand = new Random();
 
             while (true)
             {
-                var currentHeight = avgHeight + rand.NextDouble() * 4 - 2;
+                //var currentHeight = avgHeight + rand.NextDouble() * 4 - 2;
 
+                var height = client.GetHeight();
+                var id = "1";
+             
                 var telemetryDataPoint = new
                 {
-                    deviceId = "myFirstDevice",
-                    timestamp = DateTime.Now.ToString("o"), 
-                    currentHeight = currentHeight,
-                    loggedOn = true
+                    DeviceId = id,
+                    Timestamp = DateTime.Now.ToString("o"),
+                    Height = height,
+                    IsActive = IsActive
                 };
                 var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
                 var message = new Message(Encoding.ASCII.GetBytes(messageString));
