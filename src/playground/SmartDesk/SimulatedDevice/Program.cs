2948effd0f410d0b6e56411db5719c37bc6dd381
pihai <philipp-haider@aon.at>
2016-05-31 16:50:30 +0200

    first attempt to get rid of admin-dashboard theme

--- src/playground/SmartDesk/SimulatedDevice/Program.cs
+++ src/playground/SmartDesk/SimulatedDevice/Program.cs
@@ -25,35 +25,111 @@ namespace SimulatedDevice {
           await Task.Delay(TimeSpan.FromMinutes(1));
         }
       });
-      
 
-      SendDeviceToCloudMessagesAsync();
+
+      Foo().GetAwaiter().GetResult();
+
+      Console.WriteLine("done");
+      //SendDeviceToCloudMessagesAsync();
+      //Foo();
+
+
       Console.ReadLine();
+    }
+
+    private static async Task Foo() {
+      var standing = 100;
+      var sitting = 50;
+
+      TimeSpan now;
+
+      do {
+        now = DateTime.Now.TimeOfDay;
+
+        if (now.Between(7, 8)) {
+          // first sitting session
+          await SendTelemetryMessage(sitting, true);
+        }
+        else if (now.Between(8, 8.5)) {
+          // jausn -> inactiv
+          await SendTelemetryMessage(sitting, false);
+        }
+        else if (now.Between(8.5, 10)) {
+          // standing
+          await SendTelemetryMessage(standing, true);
+        } else if (now.Between(10, 11)) {
+          // standing - inactive because of meeting
+          await SendTelemetryMessage(standing, false);
+        } else if (now.Between(11, 11.25)) {
+          // standing
+          await SendTelemetryMessage(standing, true);
+        } else if (now.Between(11.25, 11.75)) {
+          // inactive - lunch
+          await SendTelemetryMessage(standing, false);
+        } else if (now.Between(11.75, 12.5)) {
+          // sitting
+          await SendTelemetryMessage(sitting, true);
+        } else if (now.Between(12.5, 14)) {
+          // standing
+          await SendTelemetryMessage(standing, true);
+        } else if (now.Between(14, 14.25)) {
+          // inactive .. coffee break
+          await SendTelemetryMessage(standing, false);
+        } else if (now > 14.25.Hours()) {
+        // sitting the rest of the day
+          await SendTelemetryMessage(sitting, true);
+        }
+        else {
+          Console.WriteLine("Nothing to do.");
+        }
+
+        await Task.Delay(TimeSpan.FromMinutes(1));
+      } while (now < 16.Hours());
     }
 
     private static async void SendDeviceToCloudMessagesAsync() {
       var standing = 100;
       var sitting = 50;
 
-      for (var j = 0; j < 2; j++) {
-        for (int i = 0; i < 3; i++) {
-          await SendTelemetryMessage(standing, true);
-          await Task.Delay(TimeSpan.FromMinutes(1));
-        }
-
-        await Task.Delay(TimeSpan.FromMinutes(3));
-
-        for (int i = 0; i < 3; i++) {
-          await SendTelemetryMessage(sitting, true);
-          await Task.Delay(TimeSpan.FromMinutes(1));
-        }
-
-        for (int i = 0; i < 3; i++) {
-          await SendTelemetryMessage(sitting, false);
-          await Task.Delay(TimeSpan.FromMinutes(1));
-        }
-
+      // 2 min standing
+      for (var i = 0; i < 3; i++) {
+        await SendTelemetryMessage(standing, true);
+        await Task.Delay(TimeSpan.FromMinutes(1));
       }
+
+      // 4 minute sitting
+      for (var i = 0; i < 4; i++) {
+        await SendTelemetryMessage(sitting, true);
+        await Task.Delay(TimeSpan.FromMinutes(1));
+      }
+
+      // 8 minute inactive
+      for (var i = 0; i < 8; i++) {
+        await SendTelemetryMessage(sitting, false);
+        await Task.Delay(TimeSpan.FromMinutes(1));
+      }
+
+
+
+      //for (var j = 0; j < 2; j++) {
+      //  for (int i = 0; i < 3; i++) {
+      //    await SendTelemetryMessage(standing, true);
+      //    await Task.Delay(TimeSpan.FromMinutes(1));
+      //  }
+
+      //  await Task.Delay(TimeSpan.FromMinutes(3));
+
+      //  for (int i = 0; i < 3; i++) {
+      //    await SendTelemetryMessage(sitting, true);
+      //    await Task.Delay(TimeSpan.FromMinutes(1));
+      //  }
+
+      //  for (int i = 0; i < 3; i++) {
+      //    await SendTelemetryMessage(sitting, false);
+      //    await Task.Delay(TimeSpan.FromMinutes(1));
+      //  }
+
+      //}
       Console.WriteLine("Finished.");
     }
     //private static async void SendDeviceToCloudMessagesAsync() {
@@ -96,10 +172,31 @@ namespace SimulatedDevice {
         Height = height,
         IsActive = isActive
       };
+
       var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
       var message = new Message(Encoding.ASCII.GetBytes(messageString));
-      await _deviceClient.SendEventAsync(message);
-      Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, message);
+      try {
+        await _deviceClient.SendEventAsync(message);
+      }
+      catch (Exception e) {
+        Console.WriteLine(e);
+        await Task.Delay(30000);
+        Console.WriteLine("retry");
+        await _deviceClient.SendEventAsync(message);
+      }
+      Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+    }
+  }
+
+  public static class Ext {
+    public static TimeSpan Hours(this double d) {
+      return TimeSpan.FromHours(d);
+    }
+    public static TimeSpan Hours(this int d) {
+      return TimeSpan.FromHours(d);
+    }
+    public static bool Between(this TimeSpan t, double start, double end) {
+      return t > start.Hours() && t < end.Hours();
     }
   }
 }
